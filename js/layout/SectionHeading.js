06fa5a3fafe16e9e755c014c324b73ce4786241e
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-04-24 16:56:20 +0300

    Section Headings

--- js/layout/SectionHeading.js
+++ js/layout/SectionHeading.js
@@ -3,14 +3,22 @@
 class SectionHeading {
     constructor(element) {
         this.element = element;
-        this.text = element.dataset.h2;
-        this.align = element.dataset.h2Align || '';
+        this.d = element.dataset;
+
+        this.text = this.d.h2;
 
         this.render();
     }
     render() {
-        const HTML = `<h2 class="${this.align}">${this.text}</h2>`;
-        
+        const classList = [];
+        this.d.h2Align ? classList.push(this.d.h2Align) : '';
+        this.d.h2Line === 'false' ? classList.push('no-line') : '';
+        this.d.h2Color ? classList.push(this.d.h2Color) : '';
+
+        const classNames = classList.length > 0 ? `class="${classList.join(' ')}"` : '';
+
+
+        const HTML = `<h2 ${classNames}>${this.text}</h2>`;
         this.element.insertAdjacentHTML('afterbegin', HTML);
     }
 }
